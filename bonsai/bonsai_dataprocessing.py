20ae8497e2a55301db00d9725aa121005f386750
dhdegroot <daanhugodegroot@gmail.com>
2025-06-01 20:33:37 +0200

    Corrected a (non-consequential) prefactor for getting the correct likelihood after rescaling the variances

--- bonsai/bonsai_dataprocessing.py
+++ bonsai/bonsai_dataprocessing.py
@@ -176,7 +176,7 @@ class SCData:
         if rescale_by_var and (originalData.ltqs is not None) and (originalData.ltqsVars is not None):
             originalData.ltqsVars /= (originalData.geneVariances[:, None])
             originalData.ltqs /= np.sqrt(originalData.geneVariances[:, None])
-            self.metadata.loglikVarCorr = - self.metadata.nCells * np.sum(
+            self.metadata.loglikVarCorr = - (self.metadata.nCells-1) * np.sum(
                 np.log(originalData.geneVariances))  # - self.metadata.nCells * self.metadata.nGenes * np.log(2* np.pi)
         elif (originalData.ltqs is not None) and (originalData.ltqsVars is not None):
             nVars = self.metadata.nGenes if self.metadata.nGenes is not None else originalData.geneVariances.shape
@@ -184,7 +184,7 @@ class SCData:
             originalData.geneVariances = np.ones(nVars) * mean_var
             originalData.ltqsVars /= mean_var
             originalData.ltqs /= np.sqrt(mean_var)
-            self.metadata.loglikVarCorr = - self.metadata.nCells * nVars * np.log(mean_var)
+            self.metadata.loglikVarCorr = - (self.metadata.nCells - 1) * nVars * np.log(mean_var)
         else:
             nVars = self.metadata.nGenes if self.metadata.nGenes is not None else originalData.geneVariances.shape
             originalData.geneVariances = np.ones(nVars)
